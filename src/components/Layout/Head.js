3d1055f6d71f6b8380a8fa3f99d99f7ecc16939d
hithesh svsk <62789585+SVSKHD@users.noreply.github.com>
2023-09-24 23:30:06 +0530

    head

---
+++ src/components/Layout/Head.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+const AquaHead = () => {
+  return (
+    <Head>
+      <link
+        rel="icon"
+        href="https://res.cloudinary.com/aquakartproducts/image/upload/v1695408028/favicon_b3l7y1.ico" // Replace with the path to your favicon image
+      />
+    </Head>
+  );
+};
+export default AquaHead;
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,14 +1,19 @@
-import { Html, Head, Main, NextScript } from 'next/document'
-import { Toaster } from "react-hot-toast"
+import { Html, Head, Main, NextScript } from "next/document";
+import { Toaster } from "react-hot-toast";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="icon"
+          href="https://res.cloudinary.com/aquakartproducts/image/upload/v1695408028/favicon_b3l7y1.ico" // Replace with the path to your favicon image
+        />
+      </Head>
       <body>
         <Toaster />
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
