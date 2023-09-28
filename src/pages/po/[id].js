e02c4d8bd0ff366ddc361a2dd9fe9c57ff4521bc
hitheshSVSK <hiteshsunkara@eworldes.com>
2023-09-28 23:27:43 +0530

    id po quotations

---
+++ src/pages/po/[id].js
@@ -0,0 +1,8 @@
+const AquaDynamicPo = () => {
+    return (
+        <>
+            <h1>Aqua-dynamic-po</h1>
+        </>
+    )
+}
+export default AquaDynamicPo
---
+++ src/pages/quotation/[id].js
@@ -0,0 +1,8 @@
+const AquaDynamicQuote = () => {
+    return (
+        <>
+            <h1>Aqua-dynamic-quote</h1>
+        </>
+    )
+}
+export default AquaDynamicQuote
