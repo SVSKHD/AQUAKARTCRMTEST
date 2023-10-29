5480f5a0e2c2494e978f9a317aafe156cf3aa31f
hitheshSVSK <hiteshsunkara@eworldes.com>
2023-10-30 00:17:53 +0530

    api

--- src/services/category.js
+++ src/services/category.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-let baseUrl = `${process.env.NEXT_PUBLIC_API_URL}/api`;
+let baseUrl = `https://aquakart.co.in/admin/crm/api`;
 
 const createCategory = async (data) => (
     await axios.post(`${baseUrl}/category`, data)
