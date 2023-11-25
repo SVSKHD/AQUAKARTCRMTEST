f2bd6cc6989a577ea7ac9f77095d4fd4e06b481e
hithesh svsk <62789585+SVSKHD@users.noreply.github.com>
2023-11-25 17:43:45 +0530

    cards and their reusabelity

--- src/components/cards/CardLayoutLayover.js
+++ src/components/cards/CardLayoutLayover.js
@@ -3,7 +3,7 @@ import PageWrapper from "../animations/pageWrapper"
 const AquaCardLayover = (props) => {
     return (
         <>
-            <div className="card shadow-lg">
+            <div className="card shadow-lg card-layover-align">
                 <div className="card-body text-dark">
                     <PageWrapper> {props.children}</PageWrapper>
                 </div>
