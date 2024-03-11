2f3416349a5466a5f12332697ba3531d5d17de54
SVSKHD <8svskhd@gmail.com>
2024-03-11 15:42:19 +0530

    get

--- src/pages/api/product/get.js
+++ src/pages/api/product/get.js
@@ -1,17 +1,30 @@
 import { createRouter } from "next-connect";
 import db from "@/Backend/Db/mongoose";
 import AquaProduct from "@/Backend/models/product";
+import AquaCategory from "@/Backend/models/category";
 
 const Router = createRouter();
+
 Router.get(async (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   await db.connectDb();
+  
   const { categoryId, id } = req.query;
+  
   if (id) {
     const productById = await AquaProduct.findById(id);
-    res.status(200).json(productById);
+    // Find related products by category, excluding the current product by ID
+    const relatedProducts = await AquaProduct.find({
+      category: productById.category,
+      _id: { $ne: id }
+    });
+    const productWithRelated = {
+      ...productById.toObject(), // Convert the Mongoose document to a plain JavaScript object
+      relatedProducts
+    };
+    res.status(200).json(productWithRelated);
   } else if (categoryId) {
     const productByCategory = await AquaProduct.find({ category: categoryId });
     res.status(200).json(productByCategory);
@@ -22,5 +35,6 @@ Router.get(async (req, res) => {
 
   db.disconnectDb();
 });
+
 
 export default Router.handler();
