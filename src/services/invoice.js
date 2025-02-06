b28f54aa858053594e225f9a4bbc92fdba7b1686
SVSKHD <8svskhd@gmail.com>
2025-02-06 07:56:59 +0530

    invoice

--- src/services/invoice.js
+++ src/services/invoice.js
@@ -1,33 +1,33 @@
 import axios from "axios";
 
-const getInvoices = async () => await axios.get(`/admin/crm/api/invoice`);
+const getInvoices = async () => await axios.get(`/api/invoice`);
 
 const createInvoice = async (data) =>
-  await axios.post(`/admin/crm/api/invoice`, data);
+  await axios.post(`/api/invoice`, data);
 
 const updateInvoice = async (id, data) =>
-  await axios.put(`/admin/crm/api/invoice?invoice=${id}`, data);
+  await axios.put(`/api/invoice?invoice=${id}`, data);
 
 const getIndividualInvoice = async (id) =>
-  await axios.get(`/admin/crm/api/invoice?invoice=${id}`);
+  await axios.get(`/api/invoice?invoice=${id}`);
 
 const getGstInvoices = async (gst) =>
-  axios.get(`/admin/crm/api/invoice?gst=${gst}`);
+  axios.get(`/api/invoice?gst=${gst}`);
 
 const getPoInvoices = async (po) =>
-  axios.get(`/admin/crm/api/invoice?po=${po}`);
+  axios.get(`/api/invoice?po=${po}`);
 
 const getQuotationInvoices = async (quotation) =>
-  axios.get(`/admin/crm/api/invoice?quotation=${quotation}`);
+  axios.get(`/api/invoice?quotation=${quotation}`);
 
 const getMonthlyInvoices = async (data) =>
-  axios.get(`/admin/crm/api/invoice?monthly=${data}`);
+  axios.get(`/api/invoice?monthly=${data}`);
 
 const getYearlyInvoices = async (data) =>
-  axios.get(`/admin/crm/api/invoice?year=${data}`);
+  axios.get(`/api/invoice?year=${data}`);
 
 const removeInvoice = async (id) =>
-  await axios.delete(`/admin/crm/api/invoice?invoice=${id}`);
+  await axios.delete(`/api/invoice?invoice=${id}`);
 
 const InvoiceOperations = () => {
   return {
